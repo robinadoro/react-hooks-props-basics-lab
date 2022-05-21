a190717fcdd69d9b0a196a4db13d652d071e7f37
Robinson Idagiza <robinadoro@gmail.com>
2022-05-21 09:10:48 +0300

    commit tree 0f1d197f6f50318dbc283c8974d64c9b52659b87

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 
 function Home(props) {
   return (
+    
     <div id="home">
       <h1 style={{ color: props.color }}>
         {props.name} is a Web Developer from {props.city}
